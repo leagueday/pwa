7fd6b5183414490dab80fe384e11ddc80fd7c7ae
Collin Monahan <llaenowyd@gmail.com>
2021-02-12 16:38:01 -0500

    remove cruft

--- src/views/LazyPodcastTitleImage.jsx
+++ src/views/LazyPodcastTitleImage.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
-import * as colors from '../styling/colors'
-import * as typography from '../styling/typography'
 import usePodcastImage from '../api/usePodcastImage'
 
-// text alt while loading
-// <div style={{color: colors.lightCharcoal, fontFamily: typography.mono, fontSize: '50%'}}>{podcast.url}</div>
 const LazyPodcastTitleImage = ({className, podcast}) => {
   const {blob} = usePodcastImage(podcast)
 
