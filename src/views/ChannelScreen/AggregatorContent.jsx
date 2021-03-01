9e773e1bb13a091915a8928397c4efca5264d1f3
Collin Monahan <llaenowyd@gmail.com>
2021-02-28 21:10:42 -0500

    reuse FacetedRow in Aggregator View

--- src/views/ChannelScreen/AggregatorContent.jsx
+++ src/views/ChannelScreen/AggregatorContent.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import { makeStyles, useTheme } from '@material-ui/core/styles'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
+// import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Grid from '@material-ui/core/Grid'
 
-import * as colors from '../../styling/colors'
+// import * as colors from '../../styling/colors'
 import useFacets from '../../api/useFacets'
-import PodcastTile from '../PodcastTile'
+// import PodcastTile from '../PodcastTile'
+import FacetedPodcastTiles from '../FacetedPodcastTiles'
 import PreviousBroadcastsMockup from './PreviousBroadcastsMockup'
 
 const useStyles = makeStyles(theme => ({
@@ -33,15 +34,15 @@ const useStyles = makeStyles(theme => ({
   previousBroadcasts: {
     marginTop: '1.5em',
   },
-  rest: { },
-  tile: {
-  },
-  tileItem: {
-    paddingRight: '1vw',
-    ['&:last-child']: {
-      paddingRight: 0,
-    },
-  },
+  // rest: { },
+  // tile: {
+  // },
+  // tileItem: {
+  //   paddingRight: '1vw',
+  //   ['&:last-child']: {
+  //     paddingRight: 0,
+  //   },
+  // },
 }))
 
 const Logo = ({channel, classes}) => {
@@ -70,75 +71,91 @@ const Headline = ({channel, classes}) => {
   )
 }
 
-const FirstPart = ({channel, classes, facetPart, nextColor}) => {
+const LogoAndTitle = ({channel, classes}) => {
   return (
-    <div className={classes.firstpart}>
-      <Headline classes={classes} channel={channel} />
-      <Grid container>
-        {
-          facetPart.map(
-            podcast => (
-              <Grid className={classes.tileItem} key={podcast.id} item xs={12} sm={6} md={6} lg={3} xl={2}>
-                <div className={classes.tile}>
-                  <PodcastTile podcast={podcast} textColor={nextColor()} />
-                </div>
-              </Grid>
-            )
-          )
-        }
+    <Grid container>
+      <Grid item sm={12} md={6}>
+        <Logo channel={channel} classes={classes} />
       </Grid>
-    </div>
+      <Grid item sm={12} md={6}>
+        <Headline channel={channel} classes={classes} />
+      </Grid>
+    </Grid>
   )
 }
 
-const Rest = ({channel, classes, facetPart, nextColor}) => {
-  return (
-    <div className={classes.rest}>
-      <Grid container>
-        {
-          facetPart.map(
-            podcast => (
-              <Grid className={classes.tileItem} key={podcast.id} item xs={12} sm={6} md={4} lg={2}>
-                <div className={classes.tile}>
-                  <PodcastTile podcast={podcast} textColor={nextColor()} />
-                </div>
-              </Grid>
-            )
-          )
-        }
-      </Grid>
-    </div>
-  )
-}
+// const FirstPart = ({channel, classes, facetPart, nextColor}) => {
+//   return (
+//     <div className={classes.firstpart}>
+//       <Headline classes={classes} channel={channel} />
+//       <Grid container>
+//         {
+//           facetPart.map(
+//             podcast => (
+//               <Grid className={classes.tileItem} key={podcast.id} item xs={12} sm={6} md={6} lg={3} xl={2}>
+//                 <div className={classes.tile}>
+//                   <PodcastTile podcast={podcast} textColor={nextColor()} />
+//                 </div>
+//               </Grid>
+//             )
+//           )
+//         }
+//       </Grid>
+//     </div>
+//   )
+// }
+//
+// const Rest = ({channel, classes, facetPart, nextColor}) => {
+//   return (
+//     <div className={classes.rest}>
+//       <Grid container>
+//         {
+//           facetPart.map(
+//             podcast => (
+//               <Grid className={classes.tileItem} key={podcast.id} item xs={12} sm={6} md={4} lg={2}>
+//                 <div className={classes.tile}>
+//                   <PodcastTile podcast={podcast} textColor={nextColor()} />
+//                 </div>
+//               </Grid>
+//             )
+//           )
+//         }
+//       </Grid>
+//     </div>
+//   )
+// }
 
 const AggregatorContent = ({channel}) => {
-  const theme = useTheme()
-  const lgUp = useMediaQuery(theme.breakpoints.up('lg'))
-
-  const firstPartNumCells = lgUp ? 4 : 2
+  // const theme = useTheme()
+  // const lgUp = useMediaQuery(theme.breakpoints.up('lg'))
+  //
+  // const firstPartNumCells = lgUp ? 4 : 2
 
   const classes = useStyles({channelColor: channel.color})
 
   const facets = useFacets(channel.tag)
   const firstFacet = facets ? facets.values().next()?.value : null
-  console.log(JSON.stringify(firstFacet, null, 2))
-  const facet = Array.isArray(firstFacet) ? firstFacet : []
+  // console.log(JSON.stringify(firstFacet, null, 2))
+  const facet = new Map([
+      [
+        '',
+        Array.isArray(firstFacet) ? firstFacet : []
+      ]
+    ])
 
-  const firstPartFacet = facet.slice(0, firstPartNumCells)
-  const restFacet = facet.slice(firstPartNumCells)
+  // const firstPartFacet = facet.slice(0, firstPartNumCells)
+  // const restFacet = facet.slice(firstPartNumCells)
 
-  const nextColor = (
-    (colorList, offset) => () => {
-      const result = colorList[offset]
-      offset = offset + 1 === colorList.length ? 0 : offset + 1
-      return result
-    }
-  )([colors.cyan, colors.blue, colors.violet, colors.magenta, colors.orange, colors.yellow], 4)
+  // const nextColor = (
+  //   (colorList, offset) => () => {
+  //     const result = colorList[offset]
+  //     offset = offset + 1 === colorList.length ? 0 : offset + 1
+  //     return result
+  //   }
+  // )([colors.cyan, colors.blue, colors.violet, colors.magenta, colors.orange, colors.yellow], 4)
 
-  return (
-    <div className={classes.aggregatorContent}>
-      <Grid className={classes.gridConMain} container>
-        <Grid item xs={12}>
+  /*
+         <Grid item xs={12}>
           <Grid className={classes.gridConTop} container>
             <Grid item xs={12} md={4} lg={3} xl={2}>
               <Logo classes={classes} channel={channel} />
@@ -150,6 +167,17 @@ const AggregatorContent = ({channel}) => {
         </Grid>
         <Grid item xs={12}>
           <Rest classes={classes} channel={channel} facetPart={restFacet} nextColor={nextColor} />
+        </Grid>
+   */
+
+  return (
+    <div className={classes.aggregatorContent}>
+      <Grid className={classes.gridConMain} container>
+        <Grid item xs={12}>
+          <LogoAndTitle channel={channel} classes={classes} />
+        </Grid>
+        <Grid item xs={12}>
+          <FacetedPodcastTiles data={facet} />
         </Grid>
         <Grid item xs={12}>
           <PreviousBroadcastsMockup
