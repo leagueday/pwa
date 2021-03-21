25c3939ef286b10e5cc7d01f7daeeea7575f57a4
Collin Monahan <llaenowyd@gmail.com>
2021-03-21 19:04:47 -0400

    setup / MyChannels seed

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,20 @@ const EventScreen = React.lazy(() => import('./views/EventScreen'))
 const IconDump = React.lazy(() => import('./views/IconDump'))
 const HomeScreen = React.lazy(() => import('./views/HomeScreen'))
 const PodcastScreen = React.lazy(() => import('./views/PodcastScreen'))
+const SetupScreen = React.lazy(() => import('./views/SetupScreen'))
 
 const matchFirstToken = match => pathTokens => pathTokens?.length > 0 && pathTokens[0] === match
 const takeNextToken = pathTokens => pathTokens.length > 1 ? pathTokens[1] : null
+const isAdminUser = userData => userData?.isAdmin ? true : false
 
 //
-// routesConfig: Array<[testRoute, View, getViewProps]>
+// routesConfig: Array<[testRoute, checkPermission, View, getViewProps]>
 //
 
 export const routesConfig = [
   [
     matchFirstToken('channel'),
+    () => true,
     ChannelScreen,
     pathTokens => ({
       channelTag: takeNextToken(pathTokens)
@@ -23,26 +26,40 @@ export const routesConfig = [
   ],
   [
     matchFirstToken('event'),
+    () => true,
     EventScreen,
     pathTokens => ({
       tag: takeNextToken(pathTokens)
-    })
+    }),
+    false,
   ],
   [
     matchFirstToken('icons'),
+    isAdminUser,
     IconDump,
-    () => ({})
+    () => ({}),
   ],
   [
     matchFirstToken('podcast'),
+    () => true,
     PodcastScreen,
     pathTokens => ({
       podcastId: takeNextToken(pathTokens)
-    })
+    }),
+  ],
+  [
+    matchFirstToken('setup'),
+    isAdminUser,
+    SetupScreen,
+    pathTokens => ({
+      subject: takeNextToken(pathTokens)
+    }),
   ],
   [
     () => true,
+    () => true,
     HomeScreen,
     () => ({}),
+    false,
   ],
 ]
