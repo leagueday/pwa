1c460a56f3d2b7ba164556f0e2ccd430bab54743
Collin Monahan <41239783+llaenowyd@users.noreply.github.com>
2021-03-22 12:53:05 -0400

    setup / MyChannels seed
    * setup / MyChannels seed
    
    * bugfix
    
    * bugfix tag/id/item in reorder
    
    * relax a bit on the grays

---
+++ src/views/SetupScreen/SetupScreen.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import {makeStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+
+import * as colors from '../../styling/colors'
+import SetupMenu from './SetupMenu'
+import SetupMyChannelsSeed from './SetupMyChannelsSeed'
+
+const contentBySubject = {
+  'seed-mylist': SetupMyChannelsSeed,
+}
+
+const useStyles = makeStyles({
+  setupScreen: {
+    backgroundColor: colors.lightGray,
+    display: 'flex',
+    flexDirection: 'row',
+    margin: '1em',
+    padding: '1em',
+    width: '100%',
+  },
+  setupContent: {
+    flex: 8,
+    overflow: 'auto',
+    padding: '0.5em',
+    width: '100%',
+  },
+})
+
+const NoContent = ({className}) => (<Card className={className} />)
+
+const SetupScreen = ({subject}) => {
+  const classes = useStyles()
+
+  const Content = contentBySubject[subject] ?? NoContent
+
+  return (
+    <Card className={classes.setupScreen}>
+      <SetupMenu subject={subject} />
+      <Content className={classes.setupContent} />
+    </Card>
+  )
+}
+
+export default SetupScreen
--- src/views/UserData.jsx
+++ src/views/UserData.jsx
@@ -2,24 +2,7 @@ import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 
 import {actions, selectors} from '../store'
-import {makeRequestHeaders} from '../api/util'
-
-const VIEW_QUERY_ENDPOINT = `/.netlify/functions/fauna-view-query`
-
-const fetchUserData = bearerToken => fetch(
-    VIEW_QUERY_ENDPOINT,
-    {
-      headers: makeRequestHeaders(bearerToken)
-    }
-  ).then(
-    res => res.headers.get('Content-Type') === 'application/json' ? res.json() : res.text()
-  ).catch(
-    err => {
-      if (NODE_ENV === 'development')
-        console.error('user data request error', err)
-      return err.message
-    }
-  )
+import fetchUserData from '../api/fetchUserData'
 
 const UserData = () => {
   const dispatch = useDispatch()
