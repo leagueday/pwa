f4a9af2ad1313269863fd3e3685a7326c5d196ee
Collin Monahan <41239783+llaenowyd@users.noreply.github.com>
2021-04-15 12:32:31 -0400

    20% reduction in SmUp Banner height (#20)

--- src/views/HomeScreen/SmUpBanner.jsx
+++ src/views/HomeScreen/SmUpBanner.jsx
@@ -23,7 +23,7 @@ const useStyles = makeStyles(theme => ({
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       height: '25%',
-      minHeight: '17em',
+      minHeight: '13.5em',
       overflow: 'hidden',
     },
   },
