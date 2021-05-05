75c642b752e4599aa47e0350dd943ca4ffc740ec
jb-reefer <james.reefer@gmail.com>
2021-05-04 22:17:57 -0400

    initial ApolloClient tests

---
+++ src/tests/call_redwood.js
@@ -0,0 +1,76 @@
+var assert = require('assert')
+const { ApolloClient, InMemoryCache, HttpLink, gql } = require('@apollo/client')
+const fetch = require('cross-fetch')
+
+const axios = require('axios')
+
+const ADDRESS = 'https://hungry-pasteur-258023.netlify.app'
+
+describe('Networking tests', function () {
+  const axiosClient = axios.create({ baseURL: ADDRESS })
+  it('Simple connection', async () => {
+    const result = await axiosClient.get('/graphql')
+    assert(result)
+  })
+
+  describe('Set up ApolloClient', () => {
+    it('Makes a call for 2 collections', async () => {
+      const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        link: new HttpLink({
+          uri: ADDRESS + '/.netlify/functions/graphql',
+          fetch,
+        }),
+      })
+
+      const result = await client.query({
+        query: gql`
+          query {
+            podcastEpisodes {
+              episode
+              url
+              title
+            }
+            podcastFeeds {
+              id
+              imageUrl
+              description
+              title
+              channelId
+            }
+          }
+        `,
+      })
+
+      assert(result)
+      assert(result.data.podcastFeeds)
+      assert(result.data.podcastEpisodes)
+    })
+  })
+
+  it('Works with example call', async () => {
+    const { ApolloClient, InMemoryCache } = require('@apollo/client')
+
+    const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: new HttpLink({
+        uri: 'https://48p1r2roz4.sse.codesandbox.io',
+        fetch,
+      }),
+    })
+
+    const result = await client.query({
+      query: gql`
+        query GetRates {
+          rates(currency: "USD") {
+            currency
+          }
+        }
+      `,
+    })
+
+    assert(result.data.rates[0])
+    assert(result.data.rates[0].__typename)
+    assert(result.data.rates[0].currency)
+  })
+})
