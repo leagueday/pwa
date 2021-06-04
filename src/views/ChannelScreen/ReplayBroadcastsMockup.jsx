73943ac432e15e0b6ea0766840844c712e9b435f
TDG <andrew.demski@demskigroup.com>
2021-06-04 13:27:46 -0400

    Player

--- src/views/ChannelScreen/ReplayBroadcastsMockup.jsx
+++ src/views/ChannelScreen/ReplayBroadcastsMockup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import cx from 'classnames'
+import ReactHlsPlayer from 'react-hls-player';
 
 import { makeStyles } from '@material-ui/core/styles'
 import useChannels from '../../api/useChannels'
