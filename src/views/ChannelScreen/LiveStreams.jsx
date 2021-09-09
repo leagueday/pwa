f76266fdebce824b26814edaf92dcdf1b76323c4
Benaiah-Varner <70243658+Benaiah-Varner@users.noreply.github.com>
2021-09-09 17:44:04 -0400

    Cleanup (#102)
    * media queries
    
    * added back previews, links turned white
    
    * changed up replay previews, cleaned up some code
    
    * added shaded background
    
    * final
    
    * removed setupscreen
    
    * a test
    
    * all fauna stuff is gone
    
    * debugging
    
    * set max length on title input
    
    * .
    
    * myList working
    
    * myList revision
    
    * final revision
    
    * last revision
    
    * worked out a problem
    
    * this better work
    
    * permissions bug fixed
    
    * better work
    
    * .
    
    * .env vars
    
    * .
    
    * api key
    
    * .env issue fixed
    
    * double add glitch
    
    * myList
    
    * livestream fix
    
    * livestream fix
    
    * profileEdit
    
    * sad
    
    * fixed
    
    * replays and hero img
    
    * profile placeholder
    
    * myList issue
    
    * myListRevision
    
    * style revision
    
    * UI changes
    
    * UI changes
    
    * fixed audio button
    
    * my channels missing on edit
    
    * all recordings on event page
    
    * airtable bug
    
    * reverse order
    
    * correct color
    
    * thats a wrap
    
    * should be good
    
    * edit profile redesign
    
    * edit profile mobile styles
    
    * removed channel info
    
    * added state provider for instant profile update
    
    * edit profile bug
    
    * refactored
    
    * edit profile changes
    
    * edit profile bug complete
    
    * revision
    
    * edit profile bug
    
    * revision
    
    * staged changes
    
    * auth and routing set up
    
    * rough draft
    
    * removed creators
    
    * fixed not signed in bug
    
    * recordings bug
    
    * profile styles edit
    
    * sizing bug
    
    * myCreators list
    
    * trophys and profile restyle
    
    * profile trophy
    
    * trophy revision
    
    * u
    
    * null
    
    * revisions
    
    * recordings
    
    * recordings
    
    * trophy fix
    
    * audio cards functional
    
    * audio card progress
    
    * LoL audiocasts
    
    * profile recordings revisions
    
    * redirect
    
    * mobile styles
    
    * seems to be working
    
    * conflicts pt2
    
    * white background
    
    * likes are good
    
    * delete and refresh
    
    * channelPlusBtn
    
    * side bar plus minus
    
    * recommendations
    
    * likes in profile
    
    * likes in profile
    
    * likes in replay page
    
    * likes and upload bugs fixed
    
    * bigs fixed
    
    * upload audiocast bug
    
    * leaguenight rec bug
    
    * twitch/twitter links
    
    * upload page
    
    * conflicts
    
    * global player
    
    * trophys
    
    * scroll bar and profile
    
    * ld creator badge
    
    * modal progress
    
    * restarting vsc
    
    * modal
    
    * duration added to modal
    
    * modal thumbnail
    
    * modal thumbnails
    
    * modal links exp.
    
    * modal done
    
    * modal mobile styles
    
    * mobile edit
    
    * descriptionModal
    
    * select
    
    * profilenavBar
    
    * profilenavBar
    
    * mobile
    
    * chat UI
    
    * revisions
    
    * revisions
    
    * lol and nav
    
    * profilenavBar
    
    * took out chat ui
    
    * audiocard mobile
    
    * mobile styles
    
    * audiocard spacing
    
    * revisions
    
    * friend requests
    
    * friend requests
    
    * friend request UI
    
    * friend req UI
    
    * like button UI
    
    * chats without socket.io
    
    * socket.io works
    
    * network issue
    
    * chat screen
    
    * selected friend
    
    * UI changes
    
    * audiocard
    
    * revisions
    
    * socket issue
    
    * socket
    
    * socket solution
    
    * Chat room issues
    
    * I think its solved
    
    * mulitple lives
    
    * replay box
    
    * replay styles fixed
    
    * live page redone
    
    * live sign on channel tiles
    
    * live sign bolded
    
    * creator bug
    
    * refactored
    
    * audiocast screen
    
    * responsive
    
    * redid input
    
    * mobile styles
    
    * revision
    
    * party chat
    
    * UI revision
    
    * comments
    
    * revision
    
    * upload audiocast error
    
    * party chat UI revision
    
    * party chat infal
    
    Co-authored-by: Benaiah Varner <benaiahvarner@MacBook-Pro.home>
    Co-authored-by: Benaiah Varner <benaiahvarner@MacBook-Pro.local>

--- src/views/ChannelScreen/LiveStreams.jsx
+++ src/views/ChannelScreen/LiveStreams.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react'
 import AudioCard from './AudioCard'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectors, actions } from '../../store'
+import('buffer').then(({ Buffer }) => {
+  global.Buffer = Buffer
+})
 import { makeStyles } from '@material-ui/styles'
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
   wrapper: {
@@ -47,8 +51,29 @@ const LiveStreams = ({ channelTag }) => {
 
   const MuxComponent = ({ livestreamid, count, audio }) => {
     const [active, setActive] = useState(false)
-    console.log('livestreamid ', livestreamid)
 
+    // const userName = 'e6dc9a66-fb63-414b-b187-6a39aaa6583f'
+    // const accessToken =
+    //   '2bGfOofUHoMPq5PtL6yb/peOp80MyN2VGsgLb5nIaREZhQ51iAtDdd4yR0pIp0bXYWWki2lcHVS'
+    // const authString = `${userName}:${accessToken}`
+    // const authStringEncoded = Buffer.from(authString).toString('base64')
+    // const headers = {
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Methods': 'POST, GET, HEAD, OPTIONS',
+    //   Authorization: `Basic ${authStringEncoded}`,
+    //   'Content-Type': 'application/json',
+    // }
+
+    // axios
+    //   .get(
+    //     'https://api.mux.com/video/v1/live-streams/mIMTxXLqtyB7mOwKWr1zavoEMjtqbqWqZ902prJY01g6k',
+    //     {
+    //       headers,
+    //     }
+    //   )
+    //   .then(res => console.log('res', res))
+    //   .catch(err => console.log(err))
     fetch('/.netlify/functions/mux-proxy', {
       method: 'POST',
       headers: {
@@ -61,6 +86,7 @@ const LiveStreams = ({ channelTag }) => {
     })
       .then(response => response.json())
       .then(function (response) {
+        console.log('esp', response)
         if (response.data.status === 'active') {
           setActive(true)
         }
@@ -71,14 +97,12 @@ const LiveStreams = ({ channelTag }) => {
 
     return (
       active && (
-        // <div onClick={() => dispatch(`/audiocast/${livestreamid}`)}>
-          <AudioCard
-            channelTag={channelTag}
-            indexData={count}
-            audio={audio}
-            live={true}
-          />
-        // </div>
+        <AudioCard
+          channelTag={channelTag}
+          indexData={count}
+          audio={audio}
+          live={true}
+        />
       )
     )
   }
@@ -86,16 +110,12 @@ const LiveStreams = ({ channelTag }) => {
   return (
     <div className={classes.wrapper}>
       {userAudio?.map((item, ind) => (
-        // <div
-          // onClick={() => dispatch(`/audiocast/${item?.fields?.liveStreamId}`)}
-        // >
-          <MuxComponent
-            livestreamid={item?.fields?.liveStreamId}
-            key={ind}
-            count={ind}
-            audio={item}
-          />
-        // </div>
+        <MuxComponent
+          livestreamid={item?.fields?.liveStreamId}
+          key={ind}
+          count={ind}
+          audio={item}
+        />
       ))}
     </div>
   )
