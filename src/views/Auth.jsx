1ece7e29ee3ed300dd1ab6317862a60ac557fd92
Benaiah-Varner <benaiahvarner@gmail.com>
2021-11-04 22:08:07 -0400

    auth bug fix

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -39,7 +39,7 @@ const Auth = () => {
     netlifyIdentity.on('error', onError)
 
     netlifyIdentity.init({
-      APIUrl: 'https://leagueday.gg/.netlify/identity'
+      APIUrl: 'https://leaguedayapp.gg/.netlify/identity'
     })
   }, [])
 
